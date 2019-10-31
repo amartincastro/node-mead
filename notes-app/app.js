43e59b250bd80412a91ed19495f57092e93e4a2e
amartincastro <castroarielm@gmail.com>
2019-10-31 14:11:40 -0700

    set up add, remove, read, and list CLI commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,50 @@
 const getNotes = require('./notes')
+const yargs = require('yargs')
 const chalk = require('chalk')
 
-const command = process.argv[2]
+//customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
 
-if (command === 'add') {
-    console.log('adding note!')
-} else if (command === 'remove') {
-    console.log('removing note!')
-}
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading the note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log('Listing all notes!')
+    }
+})
+
+
+// add, remove, read, list all notes
+// options to pass in data. 
+
+
+
+console.log(yargs.argv)
